79616d6d78957a660ed9f82d9d79b3b3bddfbaab
fjaroca <franjavaromun@gmail.com>
2017-08-10 20:41:55 +0200

    Proyecto Maven

--- 8 - Rest/src/main/java/hello/GreetingController.java
+++ 8 - Rest/src/main/java/hello/GreetingController.java
@@ -1,9 +1,9 @@
 package hello;
 
+import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GreetingController {
@@ -15,5 +15,12 @@ public class GreetingController {
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
+    }
+
+    String idUsuario="";
+    @RequestMapping("/greeting/{idUsuario}")
+    public Greeting hola(@PathVariable String idUsuario){
+
+        return new Greeting(counter.incrementAndGet(), String.format(template,idUsuario));
     }
 }
